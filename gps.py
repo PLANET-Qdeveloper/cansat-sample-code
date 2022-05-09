57ea4207326fd6e5ffbe74525177a2b7b232b5ef
Hidekazu Yazu <hidekazu419@gmail.com>
2022-05-09 22:54:19 +0900

    Update gps.py
    GPS(Micropython版)

--- gps.py
+++ gps.py
@@ -1,5 +1,5 @@
-from gps_lib import GPS
-gps = GPS(0,1,115200) # rx, tx, baudrate
+from gps_lib import GPS # ライブラリを使うためにインポートしてるよ
+gps = GPS(0,1,115200) # インスタンスの生成。rx, tx, baudrateを設定
 while True:
-    if(gps.available()):
-        print(gps.read_data())
+    if(gps.available()):    # 生成したインスタンスが正常であるかどうかチェックしているよ
+        print(gps.read_data())  # ライブラリに入っている関数read_data()を呼び出してプリントしているよ
